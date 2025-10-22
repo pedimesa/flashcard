e163c8b642683faeff18046248ed731f9bf7e48a
Renan caldas <pacheco.renan@escola.pr.gov.br>
2025-10-22 00:55:36 +0000

    finalizado

---
+++ perguntas.js
@@ -0,0 +1,103 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+
+criaCartao(
+     'lingua inglesa', 
+     'como se diz oi em inglês?',
+     'oi em inglês é HI (RAI)'
+)
+
+criaCartao(
+     'História', 
+     'Qual foi a principal causa da Revolução Francesa em 1789?',
+     'A crise financeira do Estado e as desigualdades sociais'
+)
+
+criaCartao(
+     'Educação Financeira',
+     'O que é um fundo de emergência?',
+     'O fundo de emergência é uma reserva de dinheiro que deve ser usada em casos imprevistos, como perda de emprego, problemas de saúde ou outros imprevistos financeiros.Esse fundo deve ser equivalente a pelo menos de 3 a 6 meses das suas despesas mensais.'
+)
+
+criaCartao(
+     'Projeto de vida', 
+     'Quais são meus principais objetivos de vida?',
+     'Meus objetivos de vida podem incluir aspectos como crescimento profissional, desenvolvimento pessoal, construção de relacionamentos saudáveis, a busca pela felicidade e bem-estar, ou até realizar um sonho específico como viajar, aprender algo novo ou ajudar os outros.'
+)
+
+criaCartao(
+     'Biologia I', 
+     'O que são os lipídios?',
+     'Lipídios são compostos orgânicos que incluem gorduras, óleos, ceras e fosfolipídios. Eles são uma importante fonte de energia e fazem parte das membranas celulares. Os lipídios também servem como isolantes térmicos e ajudam na proteção de órgãos.'
+)
+
+criaCartao(
+     'Fisica I', 
+     'Quais são as principais áreas de aplicação da robótica na indústria moderna?',
+     'A robótica tem diversas áreas de aplicação na indústria moderna, incluindo:'
+)
+
+criaCartao(
+     'Matemática I', 
+     'Qual a probabilidade de um dado não viciado cair com um número ímpar para cima?',
+     ' A probabilidade é de \(50\%\) (ou 0,5).'
+)
+criaCartao(
+     'Matemática II', 
+     'O que é uma variável?',
+     'É um espaço na memória do computador para armazenar dados. Cada variável possui um nome, um tipo (como número inteiro ou texto) e um valor.'
+)
+criaCartao(
+     'Biologia', 
+     'Quais são os 5 reinos em que os seres vivos são classificados?',
+     'Animalia, Plantae, Fungi, Protista e Monera.'
+)
+criaCartao(
+     'Língua Portuguesa', 
+     'Qual a função do ponto de interrogação?',
+     'Indicar uma pergunta direta ou expressar uma dúvida.'
+)
+criaCartao(
+     'Matemática', 
+     'Efetue a divisão e classifique-a em exata ou não exata: \(100\div 25\).',
+     ' A divisão é exata, pois o resultado é \(4\) e não há resto.'
+)
+criaCartao(
+     'Química', 
+     'O que é uma substância simples? .',
+     'É formada por apenas um tipo de elemento químico (ex: \(O_{2}\)) .'
+)
+criaCartao(
+     'Sociologia', 
+     'Quem é considerado o "pai da sociologia" e qual o nome que ele deu à ciência? .',
+     'Auguste Comte é considerado o "pai da sociologia". Ele originalmente chamou essa ciência de "Física Social".  .'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
